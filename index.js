74f11e1db2d40a70cca4c508a422b6c1595296f9
Hisamotokyuichi <hisamoto91@gmail.com>
2024-04-03 20:29:48 -0300

    projeto felipão

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("OLA PESSOAL");
+
+
+
+console.log("hello word");
+//input
+const nome = "isaque" ;
+let livelXp = 10000 ;
+
+
+
+    if (livelXp < 1000 )
+        {
+            console.log("O herói de nome " + nome ," está no nivel  Ferro ");
+        }else if (livelXp  <=  2000)
+        {
+        console.log("O herói de nome " + nome ," está no nivel Bronze . ");
+        }else if (livelXp <=  5000)
+        {
+        console.log("O herói de nome " + nome ," está no nivel Prata . ");
+        }else if (livelXp <=7000 )
+        {
+        console.log("O herói de nome " + nome ," está no nivel ouro . ");
+        }else if (livelXp <=8000 )
+        {
+        console.log("O herói de nome " + nome ," está no nivel plaatina . ");
+        }else if (livelXp <=9000 )
+        {
+        console.log("O herói de nome " + nome ," está no nivel Ascendente . ");
+        }else if (livelXp <=10000 )
+        {
+        console.log("O herói de nome " + nome ," está no nivel Imortal . ");
+        }else if (livelXp <=10001 )
+        {
+        console.log("O herói de nome " + nome ," está no nivel Radiante . ");
+        }
+        
+
+     
+
+
+
+
+
+   
+
+
+
+
